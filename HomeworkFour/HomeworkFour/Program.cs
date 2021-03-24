9727fdb35f6d9e28821cd3ecb945fbd0f3a93bdd
qishimu <79828499+qishimu@users.noreply.github.com>
2021-03-24 21:13:37 +0800

    Add files via upload

---
+++ HomeworkFour/HomeworkFour/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace HomeworkFour
+{
+    class Program
+    {
+        public delegate int Action(GenericList<int> List);
+        static void Main(string[] args)
+        {
+           
+
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 12; x++)
+            {
+                intlist.Add(x);
+            }
+            Action d = (GenericList<int> List) =>
+            {
+                int []  array=new int[10] ;
+                for (Node<int> node = intlist.Head ; node != null; node = node.Next)
+                {
+                    int i = 0;
+                    array[i] = node.Data;
+                    Console.WriteLine(node.Data);
+                    i++;
+                }
+                int min = array[0];
+                int max =array[0];
+                for(int i = 0; i < array.Length; i++)
+                {
+                    if(min > array[i])
+                     min = array[i];
+                    if(max < array[i])
+                    max = array[i];
+                }
+                Console.WriteLine(min);
+                Console.WriteLine(max);
+                Console.WriteLine(max+min);
+                return 0;
+            };
+            d(intlist);
+            GenericList<string> strList = new GenericList<string>();
+            for(int x = 0; x < 10; x++)
+            {
+                strList.Add("str" + x);
+            }
+            for(Node<string>node=strList.Head; node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+        }
+        
+    }
+}
+//链表节点
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+//head头 tail尾
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head
+    {
+        get => head;
+    }
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head = tail = n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail = n;
+        }
+    }
+    }
+
+
+
+  
---
+++ HomeworkFour/Two/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ra = new Random();
+            int a = ra.Next(5);
+            int b = ra.Next(10);
+            Form form1 = new Form();
+            form1.ClockOne.OnAlarm();
+            form1.ClockOne.OnTick(a, b);
+        }
+    }
+}
+public delegate void AlarmClock(object punctual, ClockTime c);//参数的思考
+
+public class ClockTime
+{
+    public int X
+    {
+        get;set;
+    }
+    public int Y
+    {
+        get;set;
+    }
+}
+public class Clock
+{
+    public event AlarmClock Alarm;
+    public event AlarmClock Tick;
+    public void OnTick(int x, int y)//触发Tick的方法
+    {
+        ClockTime arg = new ClockTime()
+        {
+            X = x,
+            Y = y
+        };
+        do
+        {
+                TimeSpan ts = new TimeSpan(1000);
+                y++;
+                Console.WriteLine("滴答" + x + ":" + y);
+            } while (x <= 5 && y < 10);
+        do
+        {
+            y = 0; x ++;
+            Console.WriteLine("滴答" + x + ":" + y);
+        } while (x != 5 && y == 10);
+        do
+        {
+            y = 0; x = 0;
+            Console.WriteLine("滴答" + x + ":" + y);
+        } while (x == 5 && y == 10);
+    }
+    public void OnAlarm()//触发Alarm的方法
+    {
+        Console.WriteLine("请设置闹铃的时间");
+        string m=Console.ReadLine();
+        int x = Convert.ToInt32(m);
+        string n = Console.ReadLine();
+        int y = Convert.ToInt32(n);
+        ClockTime arg = new ClockTime();
+        do
+        {
+          Console.WriteLine("闹铃响了,该开始了");
+        } while (x == arg.X && y ==arg. Y);
+    }
+}
+public class Form
+{
+    public Clock ClockOne = new Clock();
+    public Form()
+    {
+        ClockOne.Alarm += new AlarmClock(BeAlarm);
+        ClockOne.Tick += new AlarmClock(BeTick);
+        static void BeTick(object punctual, ClockTime c)
+        {
+
+        }
+        static void BeAlarm(object punctual, ClockTime c)
+        {
+
+        }
+    }
+}
+
+
